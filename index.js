ddcb82bbeb13846d37377629b79499300b9cab56
dinesh ubrani <dineshubrani1109@gmail.com>
2024-03-06 14:33:37 +0530

    first comit

---
+++ index.js
@@ -0,0 +1,82 @@
+const socketIO = require("socket.io");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+require("dotenv").config({
+  path: "./.env",
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world from socket server!");
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (receiverId) => {
+  return users.find((user) => user.userId === receiverId);
+};
+
+// Define a message object with a seen property
+const createMessage = ({ senderId, receiverId, text, images }) => ({
+  senderId,
+  receiverId,
+  text,
+  images,
+  seen: false,
+});
+
+io.on("connection", (socket) => {
+  // when connect
+  console.log(`a user is connected`);
+
+  // take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  // send and get message
+  const messages = {}; // Object to track messages sent to each user
+
+  socket.on("sendMessage", ({ senderId, receiverId, text, images }) => {
+    const message = createMessage({ senderId, receiverId, text, images });
+
+    const user = getUser(receiverId);
+
+    if (user && user.socketId) {
+      // Only emit the message if user.socketId is available
+      io.to(user.socketId).emit("getMessage", message);
+    } else {
+      // Handle the case when user or user.socketId is not available
+      console.error(`Failed to send message. User or socketId not available.`);
+      // Optionally, you can emit an error event or take other appropriate actions.
+    }
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log(`a user disconnected!`);
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
+
+server.listen(process.env.PORT || 4000, () => {
+  console.log(`server is running on port ${process.env.PORT || 4000}`);
+});
